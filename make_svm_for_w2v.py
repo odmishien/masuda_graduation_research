c9783b8703f0dcfa4517653a1c1c3a9394d790af
odmishien <tmtm.yagi.007@gmail.com>
2020-05-19 15:15:01 +0900

    type is intなものがエラーになる

--- make_svm_for_w2v.py
+++ make_svm_for_w2v.py
@@ -1,4 +1,11 @@
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.externals import joblib
+import os
 import numpy
+import random
+
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 import json
@@ -15,20 +22,6 @@ hot_masudas = []
 masudas = []
 hot_vectors = []
 vectors = []
-
-
-def get_nearest_index_of_value(list, num):
-    """
-    概要: リストからある値に最も近い値を返却する関数
-    @param list: データ配列
-    @param num: 対象値
-    @return 対象値に最も近い値
-    """
-
-    # リスト要素と対象値の差分を計算し最小値のインデックスを取得
-    n = numpy.asarray(num)
-    idx = numpy.abs([numpy.sqrt(numpy.sum(numpy.asarray(l) - n)**2) for l in list]).argmin()
-    return idx
 
 for i in range(1, 344):
     with open("data/hot_entry/masuda_{0}.json".format(i), "r") as f:
@@ -70,14 +63,32 @@ for i in range(2, 5001):
                 vectors.append(sum_of_vector)
                 masudas.append(masuda)
 
-for i, vec in enumerate(hot_vectors):
-    nearest_index = get_nearest_index_of_value(vectors, vec)
-    print("id:{0}, ブクマ:{1}".format(
-        hot_masudas[i]["masuda_id"], hot_masudas[i]["bookmark_count"]))
-    print(hot_masudas[i]["content"])
-    print("\n")
-    print("👑似てるエントリ👑")
-    print("id:{0}, ブクマ:{1}".format(
-        masudas[nearest_index]["masuda_id"], masudas[nearest_index]["bookmark_count"]))
-    print(masudas[nearest_index]["content"])
-    print("-------------------------------------------")
+features = []
+labels = []
+target_names = ['class 0', 'class 1']
+
+for hot_vec in hot_vectors:
+    features.append(hot_vec)
+    labels.append(1)
+for vec in vectors:
+    features.append(vec)
+    labels.append(0)
+# トレーニングデータ:テストデータ を 9:1 に分割
+data_train, data_test, label_train, label_test = train_test_split(features, labels, test_size=0.1, random_state=1)
+
+# トレーニングデータから分類器を作成 (Linear SVM)
+parameters = [
+    # {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['linear'],'tol': [0.001,0.0001], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100,1000], 'class_weight':['balanced']},
+    {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100,1000],'class_weight':['balanced']}
+    # {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['poly'],'tol': [0.001,0.0001], 'degree': [2, 3, 4], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100],'class_weight':['balanced']},
+    # {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['sigmoid'],'tol': [0.001,0.0001], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100],'class_weight':['balanced']}
+    ]
+estimator = GridSearchCV(SVC(), parameters, cv=2,n_jobs = -1,return_train_score=False)
+# estimator = SVC(kernel='rbf',C=10, tol=0.01, gamma='scale', class_weight="balanced")
+estimator.fit(data_train, label_train)
+# テストデータを分類器に入れる
+label_predict = estimator.predict(data_test)
+# Accuracy
+print(estimator.best_params_)
+print(classification_report(label_test, label_predict, target_names=target_names))
+# print(accuracy_score(label_test, label_predict))
