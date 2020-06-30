b7125786add2596038873d6680747f7149f3040c
odmishien <tmtm.yagi.007@gmail.com>
2020-06-30 13:18:49 +0900

    no_noun_and_adj

---
+++ convert_pos_to_pos_name.py
@@ -0,0 +1,50 @@
+import json
+import MeCab
+import os
+
+mecab_path = os.getenv("MECAB_NEOLOGD_PATH")
+mecab = MeCab.Tagger("-d {0}".format(mecab_path))
+
+for i in range(1, 344):
+    with open("data/hot_entry/masuda_{0}.json".format(i), "r") as f:
+        hot_masuda_objs = json.load(f)
+    converted_hot_masuda_objs = []
+    for masuda in hot_masuda_objs:
+        if masuda["content"] is not None:
+            content = ""
+            node = mecab.parseToNode(masuda["content"])
+            while node:
+                pos = node.feature.split(",")[0]
+                if pos == "名詞":
+                    content += "名詞"
+                elif pos == "形容詞":
+                    content += "形容詞"
+                else:
+                    content += node.surface
+                node = node.next
+            masuda["content"] = content
+        converted_hot_masuda_objs.append(masuda)
+    with open('./data/hot_entry_no_noun_and_adj/masuda_{page}.json'.format(page=i), 'w') as f:
+        json.dump(converted_hot_masuda_objs, f, indent=2, ensure_ascii=False)
+
+for i in range(2, 5001):
+    with open("data/entry/masuda_{0}.json".format(i), "r") as f:
+        masuda_objs = json.load(f)
+    converted_masuda_objs = []
+    for masuda in masuda_objs:
+        if masuda["content"] is not None:
+            content = ""
+            node = mecab.parseToNode(masuda["content"])
+            while node:
+                pos = node.feature.split(",")[0]
+                if pos == "名詞":
+                    content += "名詞"
+                elif pos == "形容詞":
+                    content += "形容詞"
+                else:
+                    content += node.surface
+                node = node.next
+            masuda["content"] = content
+        converted_masuda_objs.append(masuda)
+    with open('./data/entry_no_noun_and_adj/masuda_{page}.json'.format(page=i), 'w') as f:
+        json.dump(converted_masuda_objs, f, indent=2, ensure_ascii=False)
--- make_svm_for_w2v.py
+++ make_svm_for_w2v.py
@@ -23,40 +23,34 @@ hot_vectors = []
 vectors = []
 
 for i in range(1, 344):
-    with open("data/hot_entry/masuda_{0}.json".format(i), "r") as f:
+    with open("data/hot_entry_no_noun_and_adj/masuda_{0}.json".format(i), "r") as f:
         hot_masuda_objs = json.load(f)
     for masuda in hot_masuda_objs:
-        wakati_masuda = {}
         if masuda["content"] is not None:
             node = mecab.parseToNode(masuda["content"])
             sum_of_vector = 0
             while node:
-                pos = node.feature.split(",")[0]
-                if pos == "名詞":
-                    try:
-                        sum_of_vector += model[node.surface]
-                    except:
-                        pass
+                try:
+                    sum_of_vector += model[node.surface]
+                except:
+                    pass
                 node = node.next
             if type(sum_of_vector) is not int:
                 hot_vectors.append(sum_of_vector)
                 hot_masudas.append(masuda)
 
 for i in range(2, 5001):
-    with open("data/entry/masuda_{0}.json".format(i), "r") as f:
+    with open("data/entry_no_noun_and_adj/masuda_{0}.json".format(i), "r") as f:
         masuda_objs = json.load(f)
     for masuda in masuda_objs:
-        wakati_masuda = {}
         if masuda["content"] is not None:
             node = mecab.parseToNode(masuda["content"])
             sum_of_vector = 0
-            while node:
-                pos = node.feature.split(",")[0]
-                if pos == "名詞":
-                    try:
-                        sum_of_vector += model[node.surface]
-                    except:
-                        pass
+           while node:
+                try:
+                    sum_of_vector += model[node.surface]
+                except:
+                    pass
                 node = node.next
             if type(sum_of_vector) is not int:
                 vectors.append(sum_of_vector)
@@ -78,23 +72,23 @@ data_train, data_test, label_train, label_test = train_test_split(
     features, labels, test_size=0.1, random_state=1)
 
 # トレーニングデータから分類器を作成 (Linear SVM)
-parameters = [
-    {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['linear'], 'tol': [
-        0.001, 0.0001], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100, 1000], 'class_weight':['balanced']},
-    {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma':[
-        0.001, 0.01, 0.1, 1, 10, 100, 1000], 'class_weight':['balanced']},
-    {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['poly'], 'tol': [0.001, 0.0001],
-        'degree': [2, 3, 4], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100], 'class_weight':['balanced']},
-    {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['sigmoid'], 'tol': [
-        0.001, 0.0001], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100], 'class_weight':['balanced']}
-]
-estimator = GridSearchCV(SVC(), parameters, cv=2, n_jobs=-1, return_train_score=False, scoring="precision_weighted")
-# estimator = SVC(kernel='rbf', C=10, tol=0.01, gamma='scale', class_weight="balanced")
+# parameters = [
+#     {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['linear'], 'tol': [
+#         0.001, 0.0001], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100, 1000], 'class_weight':['balanced']},
+#     {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['rbf'], 'gamma':[
+#         0.001, 0.01, 0.1, 1, 10, 100, 1000], 'class_weight':['balanced']},
+#     {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['poly'], 'tol': [0.001, 0.0001],
+#         'degree': [2, 3, 4], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100], 'class_weight':['balanced']},
+#     {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'kernel': ['sigmoid'], 'tol': [
+#         0.001, 0.0001], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100], 'class_weight':['balanced']}
+# ]
+# estimator = GridSearchCV(SVC(), parameters, cv=2, n_jobs=-1, return_train_score=False, scoring="precision_weighted")
+estimator = SVC(kernel='rbf', C=10, tol=0.01, gamma='scale', class_weight="balanced")
 estimator.fit(data_train, label_train)
 # テストデータを分類器に入れる
 label_predict = estimator.predict(data_test)
 # Accuracy
-print(estimator.best_params_, flush=True)
-print(estimator.cv_results_, flush=True)
-# print(classification_report(label_test, label_predict, target_names=target_names))
+# print(estimator.best_params_, flush=True)
+# print(estimator.cv_results_, flush=True)
+print(classification_report(label_test, label_predict, target_names=target_names))
 print(accuracy_score(label_test, label_predict), flush=True)
